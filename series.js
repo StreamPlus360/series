35a89cbcbb71bd15122d2c0ffe748b099e3a958b
StreamPlus360 <streamplus360@outlook.com>
2025-01-27 18:07:47 -0300

    Create series.js

---
+++ series.js
@@ -0,0 +1,88 @@
+const posters = [
+    {
+        image: 'https://upload.wikimedia.org/wikipedia/pt/f/f6/Once_Upon_a_Time_Temporada_3_Poster.jpg',
+        name: 'Once Upon A Time: Disney+',
+        genres: ['ação', 'aventura'],
+        seasons: [
+            {
+                seasonName: 'Temporada 1',
+                episodes: [
+                    { name: 'Episódio 1', url: 'https://www.youtube.com/embed/dQw4w9WgXcQ' },
+                    { name: 'Episódio 2', url: 'https://www.youtube.com/embed/3JZ_D3ELwOQ' }
+                ]
+            }
+        ]
+    },
+    {
+        image: 'https://preview.redd.it/the-ammount-of-detail-in-this-show-behind-every-single-v0-vzw5r5nfi76e1.jpg?width=640&crop=smart&auto=webp&s=f5e17df61a753fbfd0a6be291b2b71197ecd245f',
+        name: 'Arcane: Netflix',
+        genres: ['animação', 'ação', 'drama'],
+        seasons: [
+            {
+                seasonName: 'Temporada 1',
+                episodes: [
+                    { name: 'Episódio 1: Entrando na Brincadeira', url: 'https://watch.brplayer.cc/watch?v=UBN4W1CF' },
+                    { name: 'Episódio 2: Alguns Mistérios Não Devem ser Desvendados', url: 'https://watch.brplayer.cc/watch?v=BJUIMCF6' },
+                    { name: 'Episódio 3: A violência é essencial para a mudança', url: 'https://watch.brplayer.cc/watch?v=OI3XGZ87' },
+                    { name: 'Episódio 4: Feliz Dia do Progresso!', url: 'https://watch.brplayer.cc/watch?v=EKHOLZJ7' },
+                    { name: 'Episódio 5: Todos querem ser meus inimigos', url: 'https://watch.brplayer.cc/watch?v=9VEK5HCI' },
+                    { name: 'Episódio 6: Quando as paredes desabam', url: 'https://watch.brplayer.cc/watch?v=XFVRY6DM' },
+                    { name: 'Episódio 7: O garoto salvador', url: 'https://watch.brplayer.cc/watch?v=3C8PLDWB' },
+                    { name: 'Episódio 8: Água e óleo', url: 'https://watch.brplayer.cc/watch?v=RW3FPJ1B' },
+                    { name: 'Episódio 9: O monstro que você criou', url: 'https://watch.brplayer.cc/watch?v=0S6GJR93' },
+
+                ]
+            },
+            {
+                seasonName: 'Temporada 2',
+                episodes: [
+                    { name: 'Episódio 1: O peso da coroa', url: 'https://watch.brplayer.cc/watch?v=VE97CJDY' },
+                    { name: 'Episódio 2: Ver tudo queimar', url: 'https://watch.brplayer.cc/watch?v=FMK67XUS' },
+                    { name: 'Episódio 3: Finalmente acertou o nome', url: 'https://watch.brplayer.cc/watch?v=BHRIL3QW' },
+                    { name: 'Episódio 4: Pinte a cidade de azul', url: 'https://watch.brplayer.cc/watch?v=VOEN3J1I' },
+                    { name: 'Episódio 5: Bolhas na mão e pés no chão', url: 'https://watch.brplayer.cc/watch?v=D5MUEB9Z' },
+                    { name: 'Episódio 6: A mensagem oculta no padrão', url: 'https://watch.brplayer.cc/watch?v=KTEU6S1C' },
+                    { name: 'Episódio 7: Fingir que é a primeira vez', url: 'https://watch.brplayer.cc/watch?v=821MCHT0' },
+                    { name: 'Episódio 8: Matar é um ciclo', url: 'https://watch.brplayer.cc/watch?v=TL3AI8DK' },
+                    { name: 'Episódio 9: A terra sob suas unhas', url: 'https://watch.brplayer.cc/watch?v=CNB5XFYS' },
+
+                ]
+            }
+        ]
+    },
+    {
+        image: 'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/79/96/51/19694367.jpg',
+        name: 'Os Simpsons',
+        genres: ['animação', 'comédia', 'aventura'],
+        seasons: [
+            {
+                seasonName: 'Temporada 1',
+                episodes: [
+                    { name: 'Episódio 1', url: 'https://www.youtube.com/embed/a2p5JktVY9Y' },
+                    { name: 'Episódio 2', url: 'https://www.youtube.com/embed/2xu8chV80oI' }
+                ]
+            },
+            {
+                seasonName: 'Temporada 2',
+                episodes: [
+                    { name: 'Episódio 1', url: 'https://www.youtube.com/embed/5PAXl_J6-iw' },
+                    { name: 'Episódio 2', url: 'https://www.youtube.com/embed/Cg5Yjlhgzo4' }
+                ]
+            }
+        ]
+    },
+    {
+        image: 'https://media.fstatic.com/Bs6CHzudsZrYkQE1LGmPA_Xq4es=/322x478/smart/filters:format(webp)/media/movies/covers/2019/12/cU4jHfo1Q9AEPnFqFqtuSA74gdi.jpg',
+        name: 'Bunny Girl Senpai',
+        genres: ['animação', 'comédia', 'aventura'],
+        seasons: [
+            {
+                seasonName: 'Temporada 1',
+                episodes: [
+                    { name: 'Episódio 1', url: 'https://www.youtube.com/embed/a2p5JktVY9Y' },
+                    { name: 'Episódio 2', url: 'https://www.youtube.com/embed/2xu8chV80oI' }
+                ]
+            }
+        ]
+    }
+];
